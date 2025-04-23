328eb14ccc344eccda4c83c85ee37ddb57abfe8b
KuldeepBhagat <102750747+KuldeepBhagat@users.noreply.github.com>
2025-04-23 22:00:25 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,26 @@
+function loadNews(category = 'technology') {
+  fetch(`http://localhost:3000/news?category=${category}`)
+    .then(res => res.json())
+    .then(articles => {
+      const container = document.getElementById('news-container');
+      container.innerHTML = '';
+      articles.forEach(article => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+          <h3>${article.title}</h3>
+          <img src="${article.urlToImage}" width="300">
+          <p>${article.description}</p>
+          <a href="${article.url}" target="_blank">Read more</a>
+          <hr/>
+        `;
+        container.appendChild(div);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching news:', error);
+    });
+}
+const health = document.getElementById('health');
+health.addEventListener('click', () => {
+  loadNews('health');
+})
