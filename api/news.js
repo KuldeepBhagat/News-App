099957ac6b26664b27763df50d3e62d22ba09b98
KuldeepBhagat <102750747+KuldeepBhagat@users.noreply.github.com>
2025-04-27 19:31:20 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -3,12 +3,10 @@ import NewsAPI from 'newsapi';
 const newsapi = new NewsAPI('32a810dc939a46b0aa36a50062567064');
 
 export default async function handler(req, res) {
-  const category = req.query.category || 'technology';
+  const category = req.query.category || 'geenral';
 
   try {
     const result = await newsapi.v2.topHeadlines({
-      language: 'en',
-      country: 'us',
       category,
     });
 
