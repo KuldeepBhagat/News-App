12ab52f8a75311e47bac74db7d884acca762ef93
KuldeepBhagat <102750747+KuldeepBhagat@users.noreply.github.com>
2025-05-01 21:19:50 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -3,14 +3,28 @@ import NewsAPI from 'newsapi';
 const newsapi = new NewsAPI('32a810dc939a46b0aa36a50062567064');
 
 export default async function handler(req, res) {
-  const category = req.query.category || 'geenral';
+  const category = req.query.category;
+  const source = req.query.source;
+  const q = req.query.q;
 
   try {
-    const result = await newsapi.v2.topHeadlines({
-      category,
-    });
+    let result;
+    if (source) {
+      result = await newsapi.v2.topHeadlines({
+        sources: source,
+      });
+    } else if (category) {
+      result = await newsapi.v2.topHeadlines({
+        category: category,
+      })
+    }
+    else if (q) {
+      result = await newsapi.v2.topHeadlines({
+        q: q,
+      })
+    }
 
-    res.status(200).json(result.articles);
+    res.json(result.articles);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch news' });
   }
