3904cb7409b1077719b64a33931651807d0334ba
KuldeepBhagat <102750747+KuldeepBhagat@users.noreply.github.com>
2025-06-05 13:53:21 +0530

    Update sports.js

--- api/sports.js
+++ api/sports.js
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), "sports.json");
+  const filePath = path.join(process.cwd(), "sports-data.json");
   const jsonData = JSON.parse(readFileSync(filePath, "utf8"));
 
   const option = req.query.option;
