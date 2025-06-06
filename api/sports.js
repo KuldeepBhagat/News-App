e08e792c3dbc3c257b85bacdd5219e7b581771ad
KuldeepBhagat <kuldeep496242@gmail.com>
2025-06-06 15:55:36 +0530

    integrated sports feature with database

--- api/sports.js
+++ api/sports.js
@@ -1,15 +1,23 @@
-import { readFileSync } from "fs";
-import path from "path";
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), "sports.json");
-  const jsonData = JSON.parse(readFileSync(filePath, "utf8"));
-
-  const option = req.query.option;
-
-  if (!option || !jsonData[option]) {
-    return res.status(404).json({ error: "Option not found" });
+  try {
+    await client.connect();
+    const db = client.db("NewsApp");
+    const collection = db.collection("SportsData");
+    
+    const query = req.query.option;
+    const data = await collection.findOne({}, {projection: {[query]: 1, _id: 0}});
+    res.status(200).json(data[query]);
+  }catch (error) {
+    console.error("Error fetching from MongoDB:", error)
+    res.status(500).json({error: "Internal Error"})
+  }finally {
+    await client.close();
   }
-
-  res.status(200).json(jsonData[option]);
 }
